b58f8cb2f32fe0093235cc4c071a87fe5a11126f
shibawanko0202 <97938730+shibawanko0202@users.noreply.github.com>
2023-10-17 01:38:31 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,106 +2,106 @@
 
 // ↓↓ ハンバーガーメニュー ↓↓ ---------------------- 
 
-  const hamburger = document.getElementById("hamburger");
-  const hamburger_back = document.getElementById("hamburger_back");
-  let hamburger_current = false;
+  // const hamburger = document.getElementById("hamburger");
+  // const hamburger_back = document.getElementById("hamburger_back");
+  // let hamburger_current = false;
 
-  hamburger.addEventListener('click',()=>{
+  // hamburger.addEventListener('click',()=>{
 
-    if(!hamburger_current){
-      hamburger_current = true;
-      hamburger.classList.add("apear");
-      hamburger_back.classList.add("apear");
-      document.addEventListener('mousewheel', taboo, { passive: false });
-      document.addEventListener('touchmove', taboo, { passive: false });
-    }else{
-      hamburger_current = false;
-      hamburger.classList.remove("apear");
-      hamburger_back.classList.remove("apear");
-      document.removeEventListener('mousewheel', taboo, { passive: false });
-      document.removeEventListener('touchmove', taboo, { passive: false });
-    };
+  //   if(!hamburger_current){
+  //     hamburger_current = true;
+  //     hamburger.classList.add("apear");
+  //     hamburger_back.classList.add("apear");
+  //     document.addEventListener('mousewheel', taboo, { passive: false });
+  //     document.addEventListener('touchmove', taboo, { passive: false });
+  //   }else{
+  //     hamburger_current = false;
+  //     hamburger.classList.remove("apear");
+  //     hamburger_back.classList.remove("apear");
+  //     document.removeEventListener('mousewheel', taboo, { passive: false });
+  //     document.removeEventListener('touchmove', taboo, { passive: false });
+  //   };
 
-  });
+  // });
 
 // ↓↓ ハンバーガーメニュー ↓↓ ---------------------- 
 
 
 // ↓↓ 訪問者カウンター ↓↓ ---------------------- 
 
-  const visit = document.getElementById("visit");
-  const v_counter = document.getElementById("v_counter");
+  // const visit = document.getElementById("visit");
+  // const v_counter = document.getElementById("v_counter");
 
-  let visit_count = 0;
+  // let visit_count = 0;
 
-  visit.addEventListener('click',()=>{
+  // visit.addEventListener('click',()=>{
 
-    visit_count++;
+  //   visit_count++;
     
-    if(visit_count == 1){
-      v_counter.classList.add("apear");
-    }else if(visit_count == 10){
-      visit_count = 0;
-      v_counter.classList.remove("apear");
-      v_counter.innerHTML = 1;
-    }else{
-      v_counter.innerHTML = visit_count;
-    };
+  //   if(visit_count == 1){
+  //     v_counter.classList.add("apear");
+  //   }else if(visit_count == 10){
+  //     visit_count = 0;
+  //     v_counter.classList.remove("apear");
+  //     v_counter.innerHTML = 1;
+  //   }else{
+  //     v_counter.innerHTML = visit_count;
+  //   };
     
-  });
+  // });
 
 // ↑↑ 訪問者カウンター ↑↑ ---------------------- 
 
 
 // ↓↓ メッセージカウンター ↓↓ ---------------------- 
 
-  const messe = document.getElementById("messe");
-  const m_counter = document.getElementById("m_counter");
+  // const messe = document.getElementById("messe");
+  // const m_counter = document.getElementById("m_counter");
 
-  let messe_count = 0;
+  // let messe_count = 0;
 
-  messe.addEventListener('click',()=>{
+  // messe.addEventListener('click',()=>{
 
-    messe_count++;
+  //   messe_count++;
     
-    if(messe_count == 1){
-      m_counter.classList.add("apear");
-    }else if(messe_count == 10){
-      messe_count = 0;
-      m_counter.classList.remove("apear");
-      m_counter.innerHTML = 1;
-    }else{
-      m_counter.innerHTML = messe_count;
-    };
+  //   if(messe_count == 1){
+  //     m_counter.classList.add("apear");
+  //   }else if(messe_count == 10){
+  //     messe_count = 0;
+  //     m_counter.classList.remove("apear");
+  //     m_counter.innerHTML = 1;
+  //   }else{
+  //     m_counter.innerHTML = messe_count;
+  //   };
     
-  });
+  // });
 
 // ↑↑ メッセージカウンター ↑↑ ---------------------- 
 
 
 // ↓↓ 有料コンテンツ用アナウンス ↓↓ ---------------------- 
 
-const attention = document.getElementById("attention");
-const paid_contents = document.getElementsByClassName("paid_content");
+// const attention = document.getElementById("attention");
+// const paid_contents = document.getElementsByClassName("paid_content");
 
-function taboo(e){
-  e.preventDefault();
-};
+// function taboo(e){
+//   e.preventDefault();
+// };
 
 
-for(let i = 0;i < paid_contents.length;i++){
-  paid_contents[i].addEventListener('click',()=>{
-    attention.classList.remove("hidden");
-    document.addEventListener('mousewheel', taboo, { passive: false });
-    document.addEventListener('touchmove', taboo, { passive: false });
-  });
-};
+// for(let i = 0;i < paid_contents.length;i++){
+//   paid_contents[i].addEventListener('click',()=>{
+//     attention.classList.remove("hidden");
+//     document.addEventListener('mousewheel', taboo, { passive: false });
+//     document.addEventListener('touchmove', taboo, { passive: false });
+//   });
+// };
 
-attention.addEventListener('click',()=>{
-  attention.classList.add("hidden");
-  document.removeEventListener('mousewheel', taboo, { passive: false });
-  document.removeEventListener('touchmove', taboo, { passive: false });
-});
+// attention.addEventListener('click',()=>{
+//   attention.classList.add("hidden");
+//   document.removeEventListener('mousewheel', taboo, { passive: false });
+//   document.removeEventListener('touchmove', taboo, { passive: false });
+// });
 
 // ↑↑ 有料コンテンツ用アナウンス ↑↑ ---------------------- 
 
