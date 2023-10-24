b11c9fd809b9907a7195b2403b224a37fbe53463
shibawanko0202 <97938730+shibawanko0202@users.noreply.github.com>
2023-10-24 18:12:28 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,155 +1,166 @@
 "use strict"
 
-// ↓↓ ハンバーガーメニュー ↓↓ ---------------------- 
 
-  // const hamburger = document.getElementById("hamburger");
-  // const hamburger_back = document.getElementById("hamburger_back");
-  // let hamburger_current = false;
+// コマンド設定
+const katsuya = document.getElementById("katsuya");
+const com_lists = document.getElementsByClassName(" command-list");
+let current_command = 0;
 
-  // hamburger.addEventListener('click',()=>{
+const check_box = document.getElementById("check");
+const check_text = document.getElementById("check-text");
+const check_lists = document.getElementsByClassName(" check-list");
+let current_check = 0;
+let check = false;
 
-  //   if(!hamburger_current){
-  //     hamburger_current = true;
-  //     hamburger.classList.add("apear");
-  //     hamburger_back.classList.add("apear");
-  //     document.addEventListener('mousewheel', taboo, { passive: false });
-  //     document.addEventListener('touchmove', taboo, { passive: false });
-  //   }else{
-  //     hamburger_current = false;
-  //     hamburger.classList.remove("apear");
-  //     hamburger_back.classList.remove("apear");
-  //     document.removeEventListener('mousewheel', taboo, { passive: false });
-  //     document.removeEventListener('touchmove', taboo, { passive: false });
-  //   };
-
-  // });
-
-// ↓↓ ハンバーガーメニュー ↓↓ ---------------------- 
+const flashscreen = document.getElementById("flashscreen");
+const blackout = document.getElementById("blackout");
+const main = document.getElementById("main");
+const bgi = document.getElementById("bgi");
+const frames = document.getElementsByClassName("frame");
+const textbox = document.getElementById("textbox");
+const selects = document.getElementsByName("entry.1604613071");
+const check_final = document.getElementById("check-final");
+let control = true;
 
 
-// ↓↓ 訪問者カウンター ↓↓ ---------------------- 
+// テキストの内容
+const texts = document.getElementsByClassName("text");
+let text_interval;
+const text00 = ["かつやが　あらわれた！",
+"かつやは　『けっこんしよう』　と言っている…"];
 
-  // const visit = document.getElementById("visit");
-  // const v_counter = document.getElementById("v_counter");
+let textcounter = false;
 
-  // let visit_count = 0;
+// コントロールON/OFF
 
-  // visit.addEventListener('click',()=>{
+  function control_on(){
+    control = true;
+    com_lists[current_command].classList.add("blink");
+  };
 
-  //   visit_count++;
-    
-  //   if(visit_count == 1){
-  //     v_counter.classList.add("apear");
-  //   }else if(visit_count == 10){
-  //     visit_count = 0;
-  //     v_counter.classList.remove("apear");
-  //     v_counter.innerHTML = 1;
-  //   }else{
-  //     v_counter.innerHTML = visit_count;
-  //   };
-    
-  // });
+  function control_off(){
+    control = false;
+  };
 
-// ↑↑ 訪問者カウンター ↑↑ ---------------------- 
+// コントロールON/OFF
 
 
-// ↓↓ メッセージカウンター ↓↓ ---------------------- 
+// テキスト表示アニメーション関数
 
-  // const messe = document.getElementById("messe");
-  // const m_counter = document.getElementById("m_counter");
+  function word(t){
 
-  // let messe_count = 0;
-
-  // messe.addEventListener('click',()=>{
-
-  //   messe_count++;
-    
-  //   if(messe_count == 1){
-  //     m_counter.classList.add("apear");
-  //   }else if(messe_count == 10){
-  //     messe_count = 0;
-  //     m_counter.classList.remove("apear");
-  //     m_counter.innerHTML = 1;
-  //   }else{
-  //     m_counter.innerHTML = messe_count;
-  //   };
-    
-  // });
-
-// ↑↑ メッセージカウンター ↑↑ ---------------------- 
-
-
-// ↓↓ 有料コンテンツ用アナウンス ↓↓ ---------------------- 
-
-// const attention = document.getElementById("attention");
-// const paid_contents = document.getElementsByClassName("paid_content");
-
-// function taboo(e){
-//   e.preventDefault();
-// };
-
-
-// for(let i = 0;i < paid_contents.length;i++){
-//   paid_contents[i].addEventListener('click',()=>{
-//     attention.classList.remove("hidden");
-//     document.addEventListener('mousewheel', taboo, { passive: false });
-//     document.addEventListener('touchmove', taboo, { passive: false });
-//   });
-// };
-
-// attention.addEventListener('click',()=>{
-//   attention.classList.add("hidden");
-//   document.removeEventListener('mousewheel', taboo, { passive: false });
-//   document.removeEventListener('touchmove', taboo, { passive: false });
-// });
-
-// ↑↑ 有料コンテンツ用アナウンス ↑↑ ---------------------- 
-
-
-// ↓↓ サムネイル複製 ↓↓ ---------------------- 
-
-  const personlist = document.getElementById("personlist");
-
-  function thumb_clone(){
-    
-    for(let i = 0;i < (persons.length - 1);i++){
-      let clone = personlist.firstElementChild.cloneNode(true);
-      personlist.appendChild(clone);
+    for(let i = 0;i < texts.length;i++){
+      texts[i].innerHTML = null;
     };
+
+    let len_total = 0;
+
+    for(let i = 0;i < t.length;i++){
+      let n = 0;
+      let len = t[i].length;
+      if(i >= 1){
+        len_total += len;
+      };
+
+      setTimeout(()=>{
+        text_interval = setInterval(()=>{
+          texts[i].innerHTML = t[i].slice(0,n);
+          //HTMLのtextに、変数sの０文字目からn文字までのテキストを表示する
+          if(n < len){
+            //文字を増やす処理の回数が入力された文字数を超えるまで繰り返す
+            n++;
+          } else {
+            //文字を増やす処理の回数が入力された文字数を超えた時の処理
+            clearInterval(text_interval);
+          };
+        },40);
+      },(len_total * 60));
+    };
+  };
+
+// テキスト表示アニメーション関数
+
+
+// バトル開始関数
+
+  function battle(){
+    
+    // com_lists[current_command].classList.remove("blink");
+
+    flashscreen.classList.add("in");
+
+    // 背景導入
+    bgi.classList.add("apear");
+    katsuya.classList.add("in");
+    setTimeout(()=>{
+      textbox.classList.add("apear");
+      bgi.classList.add("apear2");
+    },2000);
+     
+    // カツヤ登場
+    setTimeout(()=>{
+      word(text00);
+    },4000);
+    
+    for(let i = 0;i < frames.length;i++){
+      setTimeout(()=>{
+        frames[i].classList.add("apear");
+      },(i * 80));
+    };
+    setTimeout(()=>{
+      selects[0].checked = true;
+    },8000);
 
   };
 
-// ↑↑ サムネイル複製 ↑↑ ---------------------- 
+// バトル開始関数
 
 
-// ↓↓ サムネイル表示 ↓↓ ---------------------- 
 
-  const thumbnail_img = document.getElementsByClassName("thumbnail_img");
-  const thumbnail_info = document.getElementsByClassName("thumbnail_info");
-  const thumbnail_oneword = document.getElementsByClassName("thumbnail_oneword");
-  const prof_link = document.getElementsByClassName("prof_link");
-
-  function thumb_apear(){
-
-    for(let i = 0;i < thumbnail_img.length;i++){
-      thumbnail_img[i].style.backgroundImage = `url(${persons[i].img})`;
-      // thumbnail_info[i].innerHTML = `${persons[i].age}歳/${persons[i].place}/${persons[i].name}`;
-      thumbnail_info[i].innerHTML = `${persons[i].name}/${persons[i].place}/${persons[i].age}歳`;
-      thumbnail_oneword[i].innerHTML = persons[i].oneword;
-      prof_link[i].href = `profile.html?p=${i}`;
+// 最終チェックの表示変更
+for(let i = 0; i < selects.length; i++){
+  selects[i].addEventListener('change',() => {
+    if(selects[i].checked){
+      check_final.innerHTML = `「${selects[i].value}」で<br>よろしいですか？`;
+      if(check_final.textContent.length > 16){
+        check_final.classList.add("small");
+      }else{
+        check_final.classList.remove("small");
+      };
     };
-
-  };
-  
-  // ↑↑ サムネイル表示 ↑↑ ---------------------- 
-
-  new Promise((resolve)=>{
-
-    thumb_clone();
-    resolve();
-
-  }).then(()=>{
-
-    thumb_apear()
-    
   });
+};
+
+
+
+// ブラウザバックでも発火するpageshowをつかう(iOS対策)
+window.onpageshow = function(event) {
+  // ブラウザバックとかでキャッシュが残る恐れがあるので一旦リセット
+  flashscreen.classList.remove("in");
+  bgi.classList.remove("apear");
+  katsuya.classList.remove("in");
+  textbox.classList.remove("apear");
+  bgi.classList.remove("apear2");
+  check_text.classList.remove("ban");
+  for(let i = 0;i < texts.length;i++){
+    texts[i].innerHTML = null;
+  };
+  for(let i = 0;i < frames.length;i++){
+    frames[i].classList.remove("apear");
+  };
+  for(let i = 0;i < selects.length;i++){
+    selects[i].checked = false;
+  };
+
+  if (event.persisted) {
+    window.location.reload();
+  };
+
+  battle();
+};
+
+
+// 多重送信防止
+check_text.onclick = function(event){
+  check_text.classList.add("ban");
+};
--- js/result.js
+++ js/result.js
@@ -1,31 +1,33 @@
 "use strict"
 
+
+// ローディング画面
 const loading = document.getElementById("result-loading");
 const reload_btn = document.getElementById("reload-btn");
 
 // 選択肢の中身を定数定義
-const ans1 = "いずみ";
-const ans2 = "谷元";
-const ans3 = "いずぽん";
-const ans4 = "雪見";
+const ans1 = "『うん』";
+const ans2 = "『うなちゃづけ』";
+const ans3 = "謀ったなー！？";
+const ans4 = "あ、順番来たよ";
 
 // 選択肢の票数を定義、初期化
-var s1 = 0;	//いずみ 
-var s2 = 0;	//谷元
-var s3 = 0;	//いずぽん
-var s4 = 0;	//雪見
+var s1 = 0;	//『うん』 
+var s2 = 0;	//『うなちゃづけ』
+var s3 = 0;	//謀ったなー！？
+var s4 = 0;	//あ、順番来たよ
 
 var i = 1;
 
 // Googleスプレッドシートから値を取得して値を集計
-const api_url = "https://script.google.com/macros/s/AKfycby-d71hP6_-uZ6n02fAD60bUvNYQBtGe2VQf0jh3B2aokW0FDhcTRMGbq7sqUYoRAophQ/exec";
+const api_url = "https://script.google.com/macros/s/AKfycbzeFEiDppbsikEvnsOTT-rP059P89ABM0qOs6M1GGMq2QVh9J3Ap5d-GDa3nGTPdiundg/exec";
 fetch(api_url)
 .then(function (fetch_data) {
 	return fetch_data.json();
 })
 .then(function (json) {
 	for (i in json) {
-        //    console.log(json[i].ans);
+//    console.log(json[i].ans);
 		if(json[i].ans == ans1) {
 			s1++;
 		} else if (json[i].ans == ans2) {
@@ -35,11 +37,15 @@ fetch(api_url)
 		} else if (json[i].ans == ans4) {
 			s4++;
 		}
-    }
+	}
 	const total = s1 + s2 + s3 + s4;
 
-    const bars = document.getElementsByClassName("bar");
+	const graph = document.getElementById("graph");
+	const lines = document.getElementsByClassName("line");
+	const values = document.getElementsByClassName("value");
+	const boxs = document.getElementsByClassName("box");
 	const percents = document.getElementsByClassName("percent");
+	let rotate_switch = 1;
 
 	// ％変換
 	const select_01 = Math.round(s1 / total * 100); 
@@ -47,69 +53,80 @@ fetch(api_url)
 	const select_03 = Math.round(s3 / total * 100);
 	const select_04 = Math.round(s4 / total * 100);
 	const select_array = [select_01,select_02,select_03,select_04];
+	// 配列の最大数のインデックス
+	const max_num = Math.max(...select_array);
+	const max_select = select_array.indexOf(max_num);
 
 	let count1 = 0;
 	let count2 = 0;
 	let count3 = 0;
 	let count4 = 0;
 
-    loading.classList.add("loaded");
-    setTimeout(() => {
-        for(let i = 0;i < bars.length;i++){
-        bars[i].classList.add("rise");
-        };
-        const rise = document.getElementsByClassName("rise");
-    
-        // 棒グラフアニメーション追加
-        const bh = 8;
-        const bs = 15;
-        // rise[0].style.height = `${90 * bh}px`;
-        rise[0].style.height = `${select_01 * bh}px`;
-        rise[1].style.height = `${select_02 * bh}px`;
-        rise[2].style.height = `${select_03 * bh}px`;
-        rise[3].style.height = `${select_04 * bh}px`;
-        rise[0].style.transition = `height ${select_01 / bs}s`;
-        rise[1].style.transition = `height ${select_02 / bs}s`;
-        rise[2].style.transition = `height ${select_03 / bs}s`;
-        rise[3].style.transition = `height ${select_04 / bs}s`;
-    
-        // カウントUP用の変数
-        const anim_sec = 60; 
-        // カウントUPアニメーション
-        const countup01 = setInterval(()=>{
-        count1++;
-        percents[0].innerHTML = `${count1}<span>%</span>`;
-        if(count1 == select_01){
-            clearInterval(countup01);
-        };
-        },anim_sec);
-        const countup02 = setInterval(()=>{
-        count2++;
-        percents[1].innerHTML = `${count2}<span>%</span>`;
-        if(count2 == select_02){
-            clearInterval(countup02);
-        };
-        },anim_sec);
-        const countup03 = setInterval(()=>{
-        count3++;
-        percents[2].innerHTML = `${count3}<span>%</span>`;
-        if(count3 == select_03){
-            clearInterval(countup03);
-        };
-        },anim_sec);
-        const countup04 = setInterval(()=>{
-        count4++;
-        percents[3].innerHTML = `${count4}<span>%</span>`;
-        if(count4 == select_04){
-            clearInterval(countup04);
-        };
-        },anim_sec);
+	// グラフアニメーション用の変数
+	const anim_sec = 50;
 
-        reload_btn.classList.add("apear");
-    }, 1200);
+  loading.classList.add("loaded");
+  setTimeout(() => {
+    for(let i = 0;i < lines.length;i++){
+      lines[i].classList.add("rotate");
+    };
+    const rotates = document.getElementsByClassName("rotate");
+  
+    // 円グラフアニメーション追加
+    rotates[0].style.strokeDashoffset = `calc(440 - (440 * ${select_01}) / 100)`;
+    rotates[0].style.animation = `circleAnim ${(select_01 * anim_sec) / 1000}s forwards`;
+  
+    rotates[1].style.strokeDashoffset = `calc(440 - (440 * ${select_02}) / 100)`;
+    rotates[1].style.animation = `circleAnim ${(select_02 * anim_sec) / 1000}s forwards`;
+  
+    rotates[2].style.strokeDashoffset = `calc(440 - (440 * ${select_03}) / 100)`;
+    rotates[2].style.animation = `circleAnim ${(select_03 * anim_sec) / 1000}s forwards`;
+  
+    rotates[3].style.strokeDashoffset = `calc(440 - (440 * ${select_04}) / 100)`;
+    rotates[3].style.animation = `circleAnim ${(select_04 * anim_sec) / 1000}s forwards`;
+  
+  
+    // カウントUPアニメーション
+    const countup01 = setInterval(()=>{
+      count1++;
+      values[0].textContent = count1;
+      if(count1 == select_01){
+        clearInterval(countup01);
+      };
+    },anim_sec);
+    const countup02 = setInterval(()=>{
+      count2++;
+      values[1].textContent = count2;
+      if(count2 == select_02){
+        clearInterval(countup02);
+      };
+    },anim_sec);
+    const countup03 = setInterval(()=>{
+      count3++;
+      values[2].textContent = count3;
+      if(count3 == select_03){
+        clearInterval(countup03);
+      };
+    },anim_sec);
+    const countup04 = setInterval(()=>{
+      count4++;
+      values[3].textContent = count4;
+      if(count4 == select_04){
+        clearInterval(countup04);
+      };
+    },anim_sec);
+
+    reload_btn.classList.add("apear");
+  }, 1000);
 });
+
 
 // 更新ボタン
 reload_btn.addEventListener('click',()=>{
 	location.reload();
 });
+
+// ロード画面(時間固定)
+// setTimeout(() => {
+//   loading.classList.add("loaded");
+// }, 5000);
